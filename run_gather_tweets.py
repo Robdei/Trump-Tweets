5b7ac2fba5d9b244462720fccdaf4d2ae4e1d646
robbygottesman <robby.gottesman@gmail.com>
2020-03-21 10:43:40 -0700

    NLP analysis added to pipeline

--- run_gather_tweets.py
+++ run_gather_tweets.py
@@ -1,6 +1,16 @@
 import gather_tweets
 import nlp_functions
 
-tweets_df = gather_tweets.gather_from_archive(2015,2020)
-gather_tweets.convert_to_est(tweets_df).to_csv('Tweets_local.csv',index=False)
+start_year = 2015
+end_year = 2017
+
+# download tweet information
+tweets_df = gather_tweets.gather_from_archive(start_year,end_year)
+
+# convert to local time (EST)
+tweets_df = gather_tweets.convert_to_est(tweets_df)
+
+# determine sentiment and subjectivity
+tweets_df = nlp_functions.get_sentiment_and_subjectivity(tweets_df)
+tweets_df.to_csv('Test.csv',index=False)
 
