194abd1fa3d3fdba60167de90582caaaaa5b47d0
robbygottesman <robby.gottesman@gmail.com>
2020-04-10 10:51:25 -0700

    Download tweets from trump_aligned twitterverse users

--- non_rdt_tweets/tweepy_keys.py
+++ non_rdt_tweets/tweepy_keys.py
@@ -1,6 +1,6 @@
 # Add your tweepy credentials to this file
 
-consumer_key = "bB33j3DvIBSLnffnJ76hKSQb9"
-consumer_secret = "52R7wRyWW9RdEUqAPDb91IvAmnswm5AXFv9YazJn7FEoGxAcTR"
-access_key = "1121551422439460865-WODsbFT0nGBB2ZyWQQIMTS2KJOsXAF"
-access_secret = "ZSH32RkszUAJqj5Wlj7ulFe4ZcVVEsZKV8wLw5utaO7iq"
+consumer_key = "XXX"
+consumer_secret = "XXX"
+access_key = "XXX-XXX"
+access_secret = "XXX"
